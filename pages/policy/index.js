d7c8fe9ce4d8b763c37d7564608a7305ce3b92e4
SaKongA <guokexin2005@gmail.com>
2025-06-14 17:31:09 +0800

    MR-M: Updated User Agreement and Privacy Service

--- pages/policy/index.js
+++ pages/policy/index.js
@@ -1,10 +1,6 @@
 const app = getApp()
 
 Page({
-  data: {
-    activeTab: 'privacy' // 默认显示隐私政策
-  },
-
   onLoad: function() {
     // 页面加载时的逻辑
   },
@@ -12,16 +8,9 @@ Page({
   // 分享小程序
   onShareAppMessage: function() {
     return {
-      title: '马来租房 - 隐私政策',
+      title: '马来租房 - 隐私保护指引',
       path: '/pages/policy/index',
       imageUrl: '/assets/images/share-image.png'
     };
-  },
-
-  switchTab: function(e) {
-    const tab = e.currentTarget.dataset.tab;
-    if (tab && tab !== this.data.activeTab) {
-      this.setData({ activeTab: tab });
-    }
   }
 }) 
